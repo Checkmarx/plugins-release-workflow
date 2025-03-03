1b09d915a376e24c095ffc77b2d30fcbfc2a2247
greensd4 <greenspan1993@gmail.com>
2025-03-03 17:38:35 +0200

    Try auto release dispatcher (AST-000)

--- .github/scripts/dispatch_event.py
+++ .github/scripts/dispatch_event.py
@@ -1,17 +1,18 @@
 import os
 import requests
 
-def main():
-    repo = os.getenv('REPO')
-    cli_version = os.getenv('CLI_VERSION')
-    github_token = os.getenv('GITHUB_TOKEN')
+def trigger_github_workflow(org, repo, cli_version, github_token):
+    """
+    Trigger a GitHub Actions workflow using the repository_dispatch event.
 
-    if not repo or not github_token:
-        raise ValueError("REPO and GITHUB_TOKEN environment variables must be set.")
-
-    api_url = f"https://api.github.com/repos/{repo}/dispatches"
+    :param org: GitHub organization or user name (e.g., 'Checkmarx')
+    :param repo: Repository name (e.g., 'ast-github-action')
+    :param cli_version: The CLI version to be dispatched
+    :param github_token: Personal Access Token (PAT) with appropriate permissions
+    """
+    url = f'https://api.github.com/repos/{org}/{repo}/dispatches'
     headers = {
-        'Accept': 'application/vnd.github+json',
+        'Accept': 'application/vnd.github.v3+json',
         'Authorization': f'token {github_token}'
     }
     payload = {
@@ -21,11 +22,20 @@ def main():
         }
     }
 
-    response = requests.post(api_url, headers=headers, json=payload)
+    response = requests.post(url, json=payload, headers=headers)
     if response.status_code == 204:
-        print(f"Successfully dispatched event to {repo}")
+        print(f'Success: Dispatched event to {org}/{repo}.')
     else:
-        print(f"Failed to dispatch event to {repo}: {response.status_code} {response.text}")
+        print(f'Error: Failed to dispatch event to {org}/{repo}. Status code: {response.status_code}')
+        print(f'Response: {response.json()}')
 
 if __name__ == "__main__":
-    main()
+    GH_ORG = os.getenv('GH_ORG')
+    GH_REPO = os.getenv('GH_REPO')
+    CLI_VERSION = os.getenv('CLI_VERSION', '')
+    GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+
+    if not GH_ORG or not GH_REPO or not GITHUB_TOKEN:
+        raise ValueError("GH_ORG, GH_REPO, and GITHUB_TOKEN environment variables must be set.")
+
+    trigger_github_workflow(GH_ORG, GH_REPO, CLI_VERSION, GITHUB_TOKEN)
